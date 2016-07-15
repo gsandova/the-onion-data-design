0825d553bff552cf79d91abf0c0568ace1e6a49b
Gerald Sandoval <gsandoval16@cnm.edu>
2016-07-15 15:54:20 -0600

    initial revision

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html><html lang="en">
+
+	<head>
+		<title>My first styled page</title>
+	</head>
+
+	<body>
+
+		<!-- Main content -->
+		<h1>My first styled page</h1>
+
+		<p>Welcome to my styled page!
+
+		<p>It lacks images, but at least it has style.
+			And it has links, even if they don't go
+			anywhere&hellip;
+
+		<p>There should be more here, but I don't know
+			what yet.
+
+			<!-- Sign and date the page, it's only polite! -->
+		<address>Made 5 April 2004<br>
+			by myself.</address>
+
+	</body>
+</html>
+
